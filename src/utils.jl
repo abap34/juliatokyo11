9ab78ab2c76994bcf222452a1ad1ce587c105bc7
abap34 <uchi_ymgc@her0.jp>
2024-01-26 14:58:16 +0900

    latexify と簡約化込みの微分を追加

--- src/utils.jl
+++ src/utils.jl
@@ -16,7 +16,7 @@ end
 
 function id_generator()
     id = 0
-    return ()->(id+=1)
+    return () -> (id += 1)
 end
 
 struct Graph
@@ -103,7 +103,167 @@ function plot_graphviz(src::String)
 end
 
 
+function to_latex(ex::Expr)::String
+    @assert ex.head == :call
+    op = ex.args[1]
+    if op == :+
+        return "\\left(" * to_latex(ex.args[2]) * " + " * to_latex(ex.args[3]) * "\\right)"
+    elseif op == :-
+        return "\\left(" * to_latex(ex.args[2]) * " - " * to_latex(ex.args[3]) * "\\right)"
+    elseif op == :*
+        return to_latex(ex.args[2]) * " \\cdot " * to_latex(ex.args[3])
+    elseif op == :/
+        return "\\dfrac{" * to_latex(ex.args[2]) * "}{" * to_latex(ex.args[3]) * "}"
+    elseif op == :^
+        inner = to_latex(ex.args[2])
+        if length(inner) > 1
+            inner = "\\left(" * inner * "\\right)"
+        end
+        return inner * "^{" * to_latex(ex.args[3]) * "}"
+    else
+        throw("Unknown operator $op")
+    end
+end
+
+
+function to_latex(ex::Symbol)::String
+    return string(ex)
+end
+
+function to_latex(ex::Int64)::String
+    return string(ex)
+end
+
 macro plot(ex)
     return ex |> to_graph |> to_graphviz |> plot_graphviz
+end
+
+macro latexify(ex)
+    return ex |> to_latex
+end
+
+
+function add(args)
+    args = filter(x -> x != 0, args)
+    if length(args) == 0
+        return 0
+    elseif length(args) == 1
+        return args[1]
+    else
+        return Expr(:call, :+, args...)
+    end
+end
+
+function sub(args)
+    @assert length(args) == 2
+    if args[2] == 0
+        return args[1]
+    else
+        return Expr(:call, :-, args...)
+    end
+end
+
+function mul(args)
+    if 0 in args
+        return 0
+    end
+
+    args = filter(x -> x != 1, args)
+    # 全部 1 だった場合
+    if length(args) == 0
+        return 1
+        # 一個だけ残った場合
+    elseif length(args) == 1
+        return args[1]
+    else
+        return Expr(:call, :*, args...)
+    end
+end
+
+function div(args)
+    @assert length(args) == 2
+    if args[2] == 1
+        return args[1]
+    else
+        return Expr(:call, :/, args...)
+    end
+end
+
+function pow(args)
+    @assert length(args) == 2
+    if args[2] == 0
+        return 1
+    elseif args[2] == 1
+        return args[1]
+    else
+        return Expr(:call, :^, args...)
+    end
+end
+
+
+
+function derivative(ex::Expr)
+    op = ex.args[1]
+    if op == :+
+        return add(derivative.(ex.args[2:end]))
+    elseif op == :-
+        return sub([
+            derivative(ex.args[2]),
+            derivative(ex.args[3])
+        ])
+    elseif op == :*
+        if length(ex.args) == 3
+            return add([
+                mul([ex.args[2], derivative(ex.args[3])]),
+                mul([derivative(ex.args[2]), ex.args[3]])
+            ])
+        else
+            t = mul(ex.args[3:end])
+
+            return add([
+                mul([ex.args[2], derivative(t)]),
+                mul([derivative(ex.args[2]), t])
+            ])
+        end
+    elseif op == :/
+        return div([
+            sub([
+                mul([derivative(ex.args[2]), ex.args[3]]),
+                mul([ex.args[2], derivative(ex.args[3])])
+            ]),
+            pow([ex.args[3], 2])
+        ])
+
+    elseif op == :^
+        @assert ex.args[3] isa Int64
+        return mul([
+            ex.args[3],
+            pow([ex.args[2], ex.args[3] - 1]),
+            derivative(ex.args[2])
+        ])
+    elseif op == :sin
+        return mul([
+            :cos,
+            ex.args[2],
+            derivative(ex.args[2])
+        ])
+    elseif op == :cos
+        return mul([
+            :sin,
+            ex.args[2],
+            derivative(ex.args[2])
+        ])
+    else
+        error("not implemented")
+    end
+end
+
+derivative(ex::Symbol) = 1
+
+derivative(ex::Int64) = 0
+
+
+macro derivative(ex)
+    return derivative(ex)
 end
 
